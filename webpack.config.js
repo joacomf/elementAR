90992182239c610374813060b9dfb5a3fe95786a
Joaco <joaquinamf@gmail.com>
2018-05-01 12:22:23 -0300

    Agrega webpack y sus respectivos modulos

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  mode: 'development'
+};
