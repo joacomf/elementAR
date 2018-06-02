21f2f818bdeae04c4edf9d6539c0d5e0f056896d
Joaco <joaquinamf@gmail.com>
2018-06-02 16:39:35 -0300

    Reidenta webpack de 2 a 4 espacios

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-    contentBase: './dist'
-  },
-  mode: 'development'
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    mode: 'development'
 };
